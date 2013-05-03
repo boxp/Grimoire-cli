8de5c5d479a5927872988bb2652c946164d6609d
boxp <tiyotiyouda@gmail.com>
2013-05-04 00:17:28 +0900

    commands moved to core.clj

--- src/grimoire/core.clj
+++ src/grimoire/core.clj
@@ -29,7 +29,21 @@
       (.setOAuthAccessToken (AccessToken. (:token tokens) (:tokenSecret tokens)))
     ))
 
-    (load "grimoire/core_commands")
+  (defn post [input]
+    (try (str "Success:" (.getText (.updateStatus twitter input)))
+             (catch Exception e (println "Something has wrong."))))
+
+  (defn showtl []
+    (let [statusAll (reverse (.getHomeTimeline twitter))]
+      (loop [status statusAll i 1]
+        (if (= i 20)
+          nil
+          (do
+            (println (.getScreenName (.getUser (first status))) ":" (.getText (first status)))
+            (recur (rest status) (+ i 1)))))))
+
+  (defn reload []
+    (load-file "src/grimoire/core_commands.clj"))
 
     (loop [input (read-line)]
       (if (= "exit" input)
