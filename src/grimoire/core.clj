ea4d0205f05089bc9f33b9661835aa5aa796ff75
boxp <tiyotiyouda@gmail.com>
2013-05-03 14:45:49 +0900

    add commands

--- src/grimoire/core.clj
+++ src/grimoire/core.clj
@@ -1,42 +1,41 @@
 (ns grimoire.core
+  (:gen-class)
   (:require [grimoire.oauth :as oauth])
   (:import (twitter4j TwitterFactory)
            (twitter4j.auth AccessToken)))
 
-(println "Grimoire has started v0.0.1")
-(println "          _ ........_")
-(println "          , ´,.-==-.ヽ")
-(println "            ((ﾉﾉ))ﾉ）)")
-(println "          ハ) ﾟ ヮﾟﾉ)       Welcome!")
-(println "          ~,く__,ネﾉ)つ")
-(println "          |(ﾝ_l|,_,_ﾊ、")
-(println "          ｀~ し'.ﾌ~´")
+(defn -main []
+  (do
 
-(def consumers (load-file "keys.clj"))
-(def consumerKey (:consumerKey consumers))
-(def consumerSecret (:consumerSecret consumers))
+    (println "Grimoire has started v0.0.1")
+    (println "          _ ........_")
+    (println "          , ´,.-==-.ヽ")
+    (println "            ((ﾉﾉ))ﾉ）)")
+    (println "          ハ) ﾟ ヮﾟﾉ)       Welcome!")
+    (println "          ~,く__,ネﾉ)つ")
+    (println "          |(ﾝ_l|,_,_ﾊ、")
+    (println "          ｀~ し'.ﾌ~´")
 
-(try (def tokens (load-file "tokens.clj"))
-     (catch Exception e
-       (oauth/get-tokens)))
+    (def consumers (load-file "keys.clj"))
+    (def consumerKey (:consumerKey consumers))
+    (def consumerSecret (:consumerSecret consumers))
 
-(def twitter (doto (.getInstance (TwitterFactory.))
-  (.setOAuthConsumer consumerKey,consumerSecret)
-  (.setOAuthAccessToken (AccessToken. (:token tokens) (:tokenSecret tokens)))
-))
+    (try (def tokens (load-file "tokens.clj"))
+         (catch Exception e
+           (oauth/get-tokens)))
 
-(defn post [input]
-  (do (try (.updateStatus twitter input)
-           (catch Exception e (println "Something has wrong.")))))
+    (def twitter (doto (.getInstance (TwitterFactory.))
+      (.setOAuthConsumer consumerKey,consumerSecret)
+      (.setOAuthAccessToken (AccessToken. (:token tokens) (:tokenSecret tokens)))
+    ))
 
-(defn tweet []
-  (.updateStatus twitter (read-line)))
+    (load-file "src/grimoire/core_commands.clj")
 
-(loop [input (read-line)]
-  (if (= "exit" input)
-      (try (println "bye bye!")
-           (catch Exception e (println e)))
-      (do (print "=>")
-          (try (println (load-string input))
+    (loop [input (read-line)]
+      (if (= "exit" input)
+          (try (println "bye bye!")
                (catch Exception e (println e)))
-          (recur (read-line)))))
+          (do (print "Grimoire => ")
+              (try (println (load-string (str "(in-ns `grimoire.core) " input)))
+                   (catch Exception e (println e)))
+              (recur (read-line)))))))
