a024c60ddd541ade89d28ea7bfba8409c50f4e35
boxp <tiyotiyouda@gmail.com>
2013-05-03 22:29:56 +0900

    スタンドアロンで起動可能に

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [seesaw "1.4.2"]]
-  :resource-paths ["libs/twitter4j-core-3.0.3.jar"]
+                 [org.twitter4j/twitter4j-core"[3.0,)"]]
   :main grimoire.core
   :aot [grimoire.core])
