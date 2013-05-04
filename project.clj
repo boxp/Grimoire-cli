9f16e8779e9108befec2184408d9395aaa47ddf7
boxp <tiyotiyouda@gmail.com>
2013-05-04 11:24:09 +0900

    added twitter4j-stream

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.twitter4j/twitter4j-core"[3.0,)"]]
+                 [org.twitter4j/twitter4j-core"[3.0,)"]
+                 [org.twitter4j/twitter4j-stream"[3.0,)"]]
   :main grimoire.core
   :aot [grimoire.core])
