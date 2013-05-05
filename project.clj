1cf51307420a1662c528ff6e81db97252312056b
boxp <tiyotiyouda@gmail.com>
2013-05-06 00:34:22 +0900

    v0.0.5 updated. numbers of commands and tweet cache support.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grimoire "0.0.2-SNAPSHOT"
+(defproject grimoire "0.0.5-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
